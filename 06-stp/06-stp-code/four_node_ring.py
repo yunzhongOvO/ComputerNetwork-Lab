cfba79d6fe360e482d1293bcc962700a4a578c5f
kyrie2333 <zhongyun16@mails.ucas.ac.cn>
2019-04-08 23:42:58 +0800

    a little progress

---
+++ 06-stp/06-stp-code/four_node_ring.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.cli import CLI
+
+def clearIP(n):
+    for iface in n.intfList():
+        n.cmd('ifconfig %s 0.0.0.0' % (iface))
+
+class RingTopo(Topo):
+    def build(self):
+        b1 = self.addHost('b1')
+        b2 = self.addHost('b2')
+        b3 = self.addHost('b3')
+        b4 = self.addHost('b4')
+
+        self.addLink(b1, b2)
+        self.addLink(b1, b3)
+        self.addLink(b2, b4)
+        self.addLink(b3, b4)
+
+if __name__ == '__main__':
+    topo = RingTopo()
+    net = Mininet(topo = topo, controller = None) 
+
+    for idx in range(4):
+        name = 'b' + str(idx+1)
+        node = net.get(name)
+        clearIP(node)
+        node.cmd('./disable_offloading.sh')
+        node.cmd('./disable_ipv6.sh')
+
+        # set mac address for each interface
+        for port in range(len(node.intfList())):
+            intf = '%s-eth%d' % (name, port)
+            mac = '00:00:00:00:0%d:0%d' % (idx+1, port+1)
+
+            node.setMAC(mac, intf = intf)
+
+        node.cmd('./stp-reference > %s-output.txt 2>&1 &' % name)
+
+    net.start()
+    CLI(net)
+    net.stop()
